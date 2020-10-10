c8df718e80027c10817ae33e5b5a8252174f56f2
CandyAZY <1802851@sit.singaporetech.edu.sg>
2020-10-10 16:51:50 +0800

    Updated Calculator.cs

--- ICT3101_Calculator/Calculator.cs
+++ ICT3101_Calculator/Calculator.cs
@@ -89,11 +89,6 @@ namespace ICT3101_Calculator
         }
         public double Divide(double num1, double num2)
         {
-            //if (num1 == 0 || num2 == 0)
-            //{
-            //    throw new ArgumentException();
-            //}
-            //return (num1 / num2);
             if (num1 == 0 && num2 == 0)
             {
                 return 1;
